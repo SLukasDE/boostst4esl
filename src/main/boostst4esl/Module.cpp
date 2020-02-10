cecf0d22f59270df4b4be02b752931ad1337d476
Sven Lukas <Sven.Lukas@googlemail.com>
2020-02-10 07:59:43 +0100

    added implementation name to module interface

--- src/main/boostst4esl/Module.cpp
+++ src/main/boostst4esl/Module.cpp
@@ -52,7 +52,7 @@ Module::Module()
 	esl::module::Module::initialize(*this);
 
 	addInterface(std::unique_ptr<const esl::module::Interface>(new esl::stacktrace::Interface(
-			getId(), "", &createStacktrace)));
+			getId(), "boost-stacktrace", &createStacktrace)));
 }
 
 } /* anonymous namespace */
